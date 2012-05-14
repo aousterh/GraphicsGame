81912e0091ddddc06b26c8a4eb7a12a23dd5e8bc
Robert Timpe <rob.timpe@gmail.com>
2012-05-13 23:00:36 -0400

    got rid of SDL stuff

--- src/game.cpp
+++ src/game.cpp
@@ -24,11 +24,6 @@
 //#include <alut.h>
 
 
-//#include <SDL/SDL.h>
-//#include <SDL/SDL_mixer.h>
-
-
-
 ////////////////////////////////////////////////////////////
 // GLOBAL CONSTANTS
 ////////////////////////////////////////////////////////////
@@ -1638,23 +1633,23 @@ void GLUTCreateMenu(void)
 
 void ALinit(int *argc, char **argv)
 {
-	alutInit(argc, argv);
+	//alutInit(argc, argv);
 
 	ALCcontext *context;
 	ALCdevice *device;
- 
+
 	device = alcOpenDevice(NULL);
 	if (device == NULL)
 	{
 		printf("shit");
 	}
- 
+
 	//Create a context
 	context=alcCreateContext(device,NULL);
- 
+
 	//Set active context
 	alcMakeContextCurrent(context);
- 
+
 	alListener3f(AL_POSITION, 0, 0, 0);
 	alListener3f(AL_VELOCITY, 0, 0, 0);
 	alListener3f(AL_ORIENTATION, 0, 0, -1);
@@ -1829,50 +1824,6 @@ void GLUTInit(int *argc, char **argv)
 }
 
 
-/*
-void SDLInit()
-{
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
-		return;
-	}
-
-	int audio_rate = 22050;
-	Uint16 audio_format = AUDIO_S16SYS;
-	int audio_channels = 2;
-	int audio_buffers = 4096;
-
-	if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
-		fprintf(stderr, "Unable to initialize audio: %s\n", Mix_GetError());
-		exit(1);
-	}
-
-	Mix_Chunk *sound = NULL;
-
-	// (Note: Mix_LoadWAV() can also read AIFF, RIFF, OGG, and VOC files.)
-	sound = Mix_LoadWAV("../test.wav");
-	if(sound == NULL) {
-		fprintf(stderr, "Unable to load WAV file: %s\n", Mix_GetError());
-	}
-
-	int channel;
-
-	channel = Mix_PlayChannel(-1, sound, 0);
-	if(channel == -1) {
-		fprintf(stderr, "Unable to play WAV file: %s\n", Mix_GetError());
-	}
-
-	//cleanup can probably get rid of/move this
-	while(Mix_Playing(channel) != 0);
-
-	Mix_FreeChunk(sound);
-
-	Mix_CloseAudio();
-	SDL_Quit();
-}*/
-
-
-
 ////////////////////////////////////////////////////////////
 // SCENE READING
 ////////////////////////////////////////////////////////////
@@ -2009,8 +1960,6 @@ main(int argc, char **argv)
 {
   // Parse program arguments
   if (!ParseArgs(argc, argv)) exit(1);
-
-  //SDLInit();
 
   // Initialize GLUT
   GLUTInit(&argc, argv);
